4c4f3280a09020e63f8603be12bf8188c0e80a17
Dimitri Meimaridis <github@dmeim.com>
2026-09-16 10:47:40 -0400

    fix(practice): centralize supported preference ranges

--- src/components/typing/practice-config.ts
+++ src/components/typing/practice-config.ts
@@ -3,3 +3,5 @@ export const WORD_PRESETS = [10, 25, 50, 100, 500];
 export const MODE_SELECTOR_OPTIONS = ["kid", "zen", "time", "words", "quote"] as const;
 
 export type ModeSelectorOption = (typeof MODE_SELECTOR_OPTIONS)[number];
+
+export { TEXT_SIZE_MIN, TEXT_SIZE_MAX } from "@/lib/typing-constants";
